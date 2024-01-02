07bb6f97f4e5edade32a3a980d9caa35f2865f67
yashlondhe90960 <yashlondhe9151@gmail.com>
2024-01-03 00:03:09 +0530

    Day 02 Completed

--- GoldmanSachs/519. Random Flip Matrix/solution.java
+++ GoldmanSachs/519. Random Flip Matrix/solution.java
@@ -1,3 +1,32 @@
-// import java.util.*;
+import java.util.*;
 
 
+class Solution{
+    HashMap<Integer, Integer> map;
+    int zeroes;
+    Random rand;
+    int n, m;
+
+    public Solution(int m, int n){
+        map = new HashMap<>();
+        zeroes = m*n;
+        rand = new Random();
+        this.n = n;
+        this.m = m;
+
+    }
+
+    public int[] flip(){
+        int r = rand.nextInt(zeroes);
+        int actual = map.getOrDefault(r,r);
+        int[] ret = new int[] {actual/n, actual %n};
+        map.put(r,map.getOrDefault(zeroes-1, zeroes-1));
+        zeroes--;
+        return ret;
+    }
+
+    public void reset(){
+        map.clear();
+        zeroes = m*n;
+    }
+}
