5a13a8ff3b0bf84a4d804462b7a947a64cfc4aa7
yashlondhe90960 <yashlondhe9151@gmail.com>
2024-01-04 20:44:47 +0530

    Day 04 Completed

---
+++ GoldmanSachs/2261. K Divisible Elements Subarrays/solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+
+
+class Solution {
+    public int countDistinct(int[] nums, int k, int p) {
+        Set<String> set = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            int cnt = 0;
+            StringBuilder sb = new StringBuilder();
+
+            for(int j=i; j<nums.length;j++){
+                if(nums[j]%p == 0) cnt++;
+
+                if(cnt>k) break;
+
+                sb.append(nums[j] + "_");
+
+                set.add(sb.toString());
+
+            }
+        }
+        // for(String s:set){
+        //     System.out.println(s);
+
+            
+
+        // }
+        return set.size();
+    }
+}
